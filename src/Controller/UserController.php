b23bc538e342467dadeb0fbae105b19757573d5a
Oleksandr Kuzmenko <f3dstudiya@gmail.com>
2025-04-04 15:52:10 +0100

    first commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $hasher,
+        EntityManagerInterface $em
+    ): JsonResponse {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['email'], $data['password'])) {
+            return new JsonResponse(['error' => 'Email and password required'], 400);
+        }
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword($hasher->hashPassword($user, $data['password']));
+        $user->setRoles(['ROLE_USER']);
+
+        $em->persist($user);
+        $em->flush();
+
+        return new JsonResponse(['status' => 'User created'], 201);
+    }
+}
